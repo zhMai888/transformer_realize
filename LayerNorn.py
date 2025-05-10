5a4d971d5fff94413721a7b79d4da5463ea056f3
Zhihao Mai <zhMai1962132486@163.com>
2025-05-10 22:12:13 +0800

    写好LayerNorm

---
+++ LayerNorn.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))  # 缩放参数
+        self.beta = nn.Parameter(torch.zeros(d_model))  # 偏移参数
+        self.eps = eps  # 防止除以0
+
+    def forward(self, x):
+        # 均值和方差
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+
+        # 归一化
+        x_normalized = (x - mean) / torch.sqrt(var + self.eps)
+
+        # 进行缩放和偏移
+        return self.gamma * x_normalized + self.beta
+
